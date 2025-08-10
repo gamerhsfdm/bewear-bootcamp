9e6a53e1ad22db405c7085f47fad8fe4af8379c6
gamerhsfdm <76122031+gamerhsfdm@users.noreply.github.com>
2025-08-10 15:18:19 -0300

    feat: add product page

--- src/components/commom/product-item.tsx
+++ src/components/commom/product-item.tsx
@@ -15,13 +15,16 @@ interface ProductItemProps {
 const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
   return (
-    <Link href="/" className="flex flex-col gap-4">
+    <Link
+      href={`/product-variant/${firstVariant.slug}`}
+      className="flex flex-col gap-4"
+    >
       <Image
         src={firstVariant.imageUrl}
         alt={firstVariant.name}
-        sizes="100vh"
-        width={0}
+        sizes="100vw"
         height={0}
+        width={0}
         className="h-auto w-full rounded-3xl"
       />
       <div

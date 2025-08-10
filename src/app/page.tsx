44d12921d1271e6f0e1eb434d08d04e1e3067118
gamerhsfdm <76122031+gamerhsfdm@users.noreply.github.com>
2025-08-10 13:21:25 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelect from "@/components/commom/category-select";
 import { Header } from "@/components/commom/header";
 import ProductList from "@/components/commom/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
+import Footer from "@/components/commom/footer";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    limit: 4,
     with: {
       variants: true,
     },
@@ -43,7 +53,10 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+
+        <ProductList products={newlyCreatedProducts} title="Novidades" />
       </div>
+      <Footer/> 
     </>
   );
 };
